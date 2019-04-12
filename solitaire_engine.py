f9d61ea95e05ce4553453b12d612e75fabeb7c17
Raivo Ruikis <raivoruikis@gmail.com>
2019-04-12 22:41:07 +0100

    Changed import

--- solitaire_engine.py
+++ solitaire_engine.py
@@ -1,15 +1,15 @@
-import playingcards
+from playingcards import *
 
 
 class Solitaire():
     def __init__(self):
-        self.deck = playingcards.Deck()
+        self.deck = Deck()
         self.foundations = []
-        for suit in playingcards.SUITS:
-            self.foundations.append(playingcards.CardPile())
+        for suit in SUITS:
+            self.foundations.append(CardPile())
         self.tableus = []
         for tableu in range(7):
-            self.tableus.append(playingcards.Tableu())
+            self.tableus.append(Tableu())
         self.deck.shuffle()
         for i, tableu in enumerate(self.tableus):
             cards = 0
@@ -17,7 +17,7 @@ class Solitaire():
                 tableu.add(self.deck.draw())
                 cards += 1
             tableu.get_top_card().reveal()
-        self.waste = playingcards.CardPile()
+        self.waste = CardPile()
 
     def draw(self):
         self.waste.add(self.deck.draw())
